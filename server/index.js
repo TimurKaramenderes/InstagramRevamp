340d43f199a9033220a7f6407272a706726f31fb
Arianna Telesca <arianna.tlsc@gmail.com>
2022-09-23 09:57:39 +0200

    back

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const mongoDB = process.env.DBurl;
+// const exeRouter = require ('./routes/routes.js');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// app.use('/', exeRouter);
+
+mongoose.connect(mongoDB);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', ()=>{
+    console.log('database connected');
+})
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Instagram')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
