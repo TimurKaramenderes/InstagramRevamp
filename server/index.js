fd3b9af2f27f09ad96ead6e412182d964297d303
Arianna Telesca <arianna.tlsc@gmail.com>
2022-09-28 10:38:06 +0200

    react app

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ database.once('connected', ()=>{
     console.log('database connected');
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.send('Instagram')
