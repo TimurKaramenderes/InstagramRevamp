d370b192fc3c7943d3eec5716701a106a83d0956
Timur Karamenderes <timurkaramenderes@babings.studio>
2022-10-05 13:17:39 +0200

    styled component

--- client/src/component/Feed/Hall.js
+++ client/src/component/Feed/Hall.js
@@ -1,18 +1,31 @@
 import Post from "./Post"
 import Header from "../Header/Header.js"
 import styles from "./Hall.module.css"
+import styled from 'styled-components'
 
 
 
 
 
 const Hall = () => {
+
+    const PostCompo = ({className, children}) =>(
+    <div className={className}>
+        {children}
+    </div>
+);
+
+const StyledPost = styled(PostCompo)`
+    margin-top: 95px;
+    background: red;
+`;
     
     return (
         <div>
             <Header/>
-            <Post className={styles.container} />
+            <StyledPost />
             <Post />
+            <Post />    
             <Post />
         </div>
         
